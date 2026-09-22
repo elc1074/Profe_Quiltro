98d481501910805b74f9803022d335cd66040bea
Daiane <daianevieira.ufsm@gmail.com>
2026-09-22 14:16:59 -0300

    fix: correcao analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,8 +10,6 @@ import I18nManager from "./i18n/manager";
 
 import { useThemeStore } from './stores/theme'
 
-import {inject } from '@vercel/analytics'
-
 const app = createApp(App);
 I18nManager.init(i18n);
 
@@ -19,8 +17,6 @@ app
     .use(createPinia())
     .use(router)
     .use(i18n); 
-
-inject()
 
 app.mount('#app')
 
